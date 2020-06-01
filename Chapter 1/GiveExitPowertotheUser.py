fbb9427afe349527deb9c00e50d8f12b474e0ee2
André Fettouhi <A.Fettouhi@gmail.com>
2020-06-01 06:12:52 +0200

    Added solutions to Challenges in Chapter 1 and added more code examples from Chapter 1 as well.

---
+++ Chapter 1/GiveExitPowertotheUser.py
@@ -0,0 +1,17 @@
+"""
+#5: Give Exit Power to the User
+
+Run until exit layout
+"""
+
+
+def fun():
+    print('I am in an endless loop')
+
+
+if __name__ == '__main__':
+    while True:
+        fun()
+        answer = input('Do you want to exit? (y) for yes ')
+        if answer == 'y':
+            break
